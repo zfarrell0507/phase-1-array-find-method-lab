fb064ebdfd27a9a9a79b989120d266c633bec45a
Zachary Farrell <zfarrell0507@gmail.com>
2022-07-21 12:16:30 -0400

    commit tree 2ee4b4f54fbac4b18137a572d869be4eb396091c

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+// let superbowlWin = record.find(record => record.result === "W")
+
+
+let superbowlWin = (record) => {
+    const result = record.find(record => record.result === "W" );
+    return !!result ? result.year : undefined;
+}
